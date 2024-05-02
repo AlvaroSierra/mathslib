6dce788c618b7141e9bf3bf205e858f87164a9ac
Alvaro Sierra Castro <alvarosierracastro@hotmail.com>
2024-05-02 11:53:43 +0100

    feat: Length and great circle functions

--- src/geometry/line/mod.rs
+++ src/geometry/line/mod.rs
@@ -1,7 +1,8 @@
-use crate::{generals::tensor::MathVec, geometry::Intersect};
+use crate::{generals::tensor::{MathVec, MathVecTrait}, geometry::Intersect};
 
-use super::geodetic::{Bearing, GeodeticCoordinate2D};
+use super::geodetic::{earth_radius, Bearing, GeodeticCoordinate2D, GeodeticCoordinate2DTrait, NVector};
 
+// TODO: Rename to segment or alike
 pub struct Line<Coordinate> {
     x0: Coordinate,
     x1: Coordinate,
@@ -24,8 +25,37 @@ impl Line<GeodeticCoordinate2D> {
     }
 }
 
-impl<Coordinate> Intersect<Coordinate> for Line<Coordinate> {
+impl<Coordinate: GeodeticCoordinate2DTrait> Intersect<Coordinate> for Line<Coordinate> {
     fn intersects(&self, line: Line<Coordinate>) -> Vec<Coordinate> {
         todo!()
+    }
+}
+
+pub trait Length {
+    fn length(&self) -> f32;
+}
+
+impl<Coordinate: Into<NVector> + Clone> Length for Line<Coordinate> {
+    fn length(&self) -> f32 {
+        let a: NVector = self.x0.clone().into();
+        let b: NVector = self.x1.clone().into();
+
+        let numerator: f32 = a.cross_product(b).magnitude();
+        let denominator: f32 = a.dot_product(b);
+
+        earth_radius * (numerator / denominator).atan()
+    }
+}
+
+pub(crate) trait GreatCircle {
+    fn great_circle(self) -> NVector;
+}
+
+impl<Coordinate: Into<NVector> + Clone> GreatCircle for Line<Coordinate> {
+    fn great_circle(self) -> NVector{
+        let x0: NVector = self.x0.into();
+        let x1: NVector = self.x1.into();
+
+        x0.cross_product(x1)
     }
 }
