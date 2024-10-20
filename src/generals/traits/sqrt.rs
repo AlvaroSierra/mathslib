044fc6b036711d12d8967c74bdecbf47786931b5
Alvaro Sierra Castro <alvarosierracastro@hotmail.com>
2024-10-20 17:47:22 +0100

    fix: Remove unnecessary generic from sqrt trait implementation

--- src/generals/traits/sqrt.rs
+++ src/generals/traits/sqrt.rs
@@ -1,21 +1,21 @@
-pub fn sqrt<T: Sqrt<T>>(val: T) -> T {
+pub fn sqrt<T: Sqrt>(val: T) -> T {
     val.sqrt()
 }
 
-pub trait Sqrt<VAL> {
-    fn sqrt(self) -> VAL;
+pub trait Sqrt {
+    fn sqrt(self) -> Self;
 }
 
 macro_rules! impl_sqrt {
-    ($t_self:ty, $mthd:expr) => {
-        impl Sqrt<$t_self> for $t_self {
+    ($t_self:ty) => {
+        impl Sqrt for $t_self {
             #[inline]
             fn sqrt(self) -> $t_self {
-                ($mthd)(self)
+                self.sqrt()
             }
         }
     };
 }
 
-impl_sqrt!(f32, f32::sqrt);
-impl_sqrt!(f64, f64::sqrt);
+impl_sqrt!(f32);
+impl_sqrt!(f64);
