72ed982d4e5b7739198ee3de7cec5b54cbaf42f0
Alvaro Sierra Castro <alvarosierracastro@hotmail.com>
2024-11-07 21:29:51 +0000

    feat: Generalize the line segment

--- src/geometry/coordinates/cartesian.rs
+++ src/geometry/coordinates/cartesian.rs
@@ -1,4 +1,5 @@
 use crate::generals::tensor::MathVec;
+use crate::generals::traits::Zero;
 
 // In this library a vector is the same as a Cartesian coordinates point
 pub type CartesianCoordinates2D<T> = MathVec<T, 2>;
@@ -14,9 +15,19 @@ impl<T: Copy> CartesianCoordinates2D<T> {
     }
 }
 
-pub struct Segment(CartesianCoordinates2D<f64>, CartesianCoordinates2D<f64>);
+pub struct Segment<T>(CartesianCoordinates2D<T>, CartesianCoordinates2D<T>);
 
-pub fn do_intersect(p: Segment, q: Segment) -> bool {
+pub fn do_intersect<
+    T: std::cmp::Ord
+        + Zero
+        + std::ops::SubAssign
+        + Copy
+        + std::ops::Mul<Output = T>
+        + std::ops::Sub<Output = T>,
+>(
+    p: Segment<T>,
+    q: Segment<T>,
+) -> bool {
     let o1 = orientation(p.0, q.0, p.1);
     let o2 = orientation(p.0, q.0, q.1);
     let o3 = orientation(p.1, q.1, p.0);
@@ -52,34 +63,42 @@ enum Orientation {
     Collinear,
 }
 
-fn orientation(
-    p: CartesianCoordinates2D<f64>,
-    q: CartesianCoordinates2D<f64>,
-    r: CartesianCoordinates2D<f64>,
+fn orientation<
+    T: std::ops::Sub<Output = T>
+        + std::ops::Mul<Output = T>
+        + Copy
+        + std::ops::SubAssign
+        + Zero
+        + PartialEq<T>
+        + PartialOrd,
+>(
+    p: CartesianCoordinates2D<T>,
+    q: CartesianCoordinates2D<T>,
+    r: CartesianCoordinates2D<T>,
 ) -> Orientation {
-    let pq = (q - p).add_dim(0f64);
-    let qr = (r - q).add_dim(0f64);
+    let pq = (q - p).add_dim(T::zero());
+    let qr = (r - q).add_dim(T::zero());
     let n = pq.cross_product(qr);
     let p = n.data().get(2).unwrap();
 
-    if *p == 0f64 {
+    if *p == T::zero() {
         return Orientation::Collinear;
     }
 
-    if *p > 0f64 {
+    if *p > T::zero() {
         return Orientation::Clockwise;
     }
 
     Orientation::Counterclockwise
 }
 
-fn on_segment(
-    p: &CartesianCoordinates2D<f64>,
-    q: &CartesianCoordinates2D<f64>,
-    r: &CartesianCoordinates2D<f64>,
+fn on_segment<T: Copy + std::cmp::Ord>(
+    p: &CartesianCoordinates2D<T>,
+    q: &CartesianCoordinates2D<T>,
+    r: &CartesianCoordinates2D<T>,
 ) -> bool {
-    (*q.x() <= p.x().max(*r.x()))
-        && (*q.x() >= p.x().min(*r.x()))
-        && (*q.y() <= p.y().max(*r.y()))
-        && (*q.y() >= p.y().min(*r.y()))
+    (*q.x() <= *p.x().max(r.x()))
+        && (*q.x() >= *p.x().min(r.x()))
+        && (*q.y() <= *p.y().max(r.y()))
+        && (*q.y() >= *p.y().min(r.y()))
 }
